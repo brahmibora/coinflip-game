f31b4349a7e6589f1f2966d9742cc38b9c0fd186
Brahmi Bora <149796188+brahmibora@users.noreply.github.com>
2024-08-26 18:20:01 +0530

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,13 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    const CoinFlip = await ethers.getContractFactory("CoinFlip");
+    const coinFlip = await CoinFlip.deploy();
+    await coinFlip.deployed();
+    console.log("CoinFlip deployed to:", coinFlip.address);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
